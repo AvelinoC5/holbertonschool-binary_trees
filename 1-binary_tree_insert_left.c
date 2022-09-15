8454315fd4ba382170150abddb1c944eda26482c
AvelinoC5 <avelino_63@hotmail.com>
2022-09-15 16:03:11 -0500

    update 1 task

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,22 +10,21 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node = NULL;
 
-    if (!parent)
-        return (NULL);
-    
-    new_node = binary_tree_node(parent, value);
-    if (!new_node)
-        return (NULL);
-    
-    new_node->left = parent->left;
+	if (!parent)
+		return (NULL);
 
-    if (parent->left)
-        new_node->left->parent = new_node;
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
 
-    parent->left = new_node;
+	new_node->left = parent->left;
 
-    return (new_node);
+	if (parent->left)
+		new_node->left->parent = new_node;
 
+	parent->left = new_node;
+
+	return (new_node);
 }
